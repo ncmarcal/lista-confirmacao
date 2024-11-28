8e40b783b96b1eeeadca7688a4cca0edd8c93c5b
ncmarcal <nathancori@hotmail.com>
2024-11-27 22:15:55 -0300

    feature: criar código base do projeto

---
+++ src/test/java/com/example/lista_confirmacao/ListaConfirmacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lista_confirmacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaConfirmacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
