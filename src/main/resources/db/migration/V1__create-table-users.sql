8ab401827cbdb22171a2108f88aa2667b90e4469
ncmarcal <nathancori@hotmail.com>
2024-12-03 19:58:46 -0300

    feature: criar coluna presence na tabela users e modificar o id para auto-incrementar

--- src/main/resources/db/migration/V1__create-table-users.sql
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -1,6 +1,11 @@
+CREATE SEQUENCE users_id_seq;
+
 CREATE TABLE users (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY DEFAULT nextval('users_id_seq'),
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    role TEXT NOT NULL
+    role TEXT NOT NULL,
+    presence BIT NOT NULL
 );
+
+SELECT setval('users_id_seq', COALESCE((SELECT MAX(id) FROM users), 1));
