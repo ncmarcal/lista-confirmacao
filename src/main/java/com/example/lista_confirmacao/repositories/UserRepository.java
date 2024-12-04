4ac320dc6f69297fbfd78497bb01030efd1e6864
ncmarcal <nathancori@hotmail.com>
2024-12-04 20:32:37 -0300

    feature: criar repository para buscar usuário pelo username

--- src/main/java/com/example/lista_confirmacao/repositories/UserRepository.java
+++ src/main/java/com/example/lista_confirmacao/repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package com.example.lista_confirmacao.repositories;
 
-public interface UserRepository {
+import com.example.lista_confirmacao.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    UserDetails findByUsername(String username);
+
 }
